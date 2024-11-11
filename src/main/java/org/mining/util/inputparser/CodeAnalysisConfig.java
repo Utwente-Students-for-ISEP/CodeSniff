8923812d6973f9e1ba19516909e9c77346e28a07
LastAdequateCoder <baranovgregor@gmail.com>
2024-11-11 11:23:03 +0100

    Enable is set true by default

--- src/main/java/org/mining/util/inputparser/CodeAnalysisConfig.java
+++ src/main/java/org/mining/util/inputparser/CodeAnalysisConfig.java
@@ -1,6 +1,7 @@
 package org.mining.util.inputparser;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Getter;
 
 @Getter
@@ -13,12 +14,17 @@ public class CodeAnalysisConfig {
 
     @Getter
     public static class MetricConfig {
-        private boolean enabled;
+        private boolean enabled = true;
         private Integer maxMethodLength;
         private Integer classSizeThreshold;
         private Integer maxParameters;
         private Integer maxInheritanceDepth;
         private Integer maxCoupling;
+
+        @JsonSetter("enabled")
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
     }
 
     @Getter
@@ -33,6 +39,12 @@ public class CodeAnalysisConfig {
 
     @Getter
     public static class LanguageConfig {
-        private boolean enabled;
+        // Each language config is enabled by default
+        private boolean enabled = true;
+
+        @JsonSetter("enabled")
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
     }
 }
